10b75dc33a4cd6835c12eee0f9ae50f08cb8f409
Sai Yeshwanth Muddaragadda <84711595+sai2yeshwanth@users.noreply.github.com>
2023-10-06 18:08:16 +0000

    done

---
+++ index.js
@@ -0,0 +1,7 @@
+// Create the greet arrow function here and export it as default.
+const greet =(name) => `Hello, ${name}!`
+export default greet;
+// Sample Tests for your greet function
+console.log(greet("Neo"));
+console.log(greet("Trinity"));
+console.log(greet("Morpheus"));
